cece21dee0f87bb2803a99bb2a55f79ebd81c86f
aschlecht1 <33679797+aschlecht1@users.noreply.github.com>
2018-11-12 06:10:17 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <AssetTracker.h>
 
-/*******************************************************
- * Button hit logger
- * Embedded Systems Design Lab 10/26/2017            **/
-
 // assign SETUP button's pin
 int button = BTN;
 //Calling constructor from AssetTracker.cpp
@@ -15,16 +11,11 @@ AssetTracker locationTracker = AssetTracker();
 //Calling constructor from Adafruit_VEML6070.cpp
 Adafruit_VEML6070 ultraVData = Adafruit_VEML6070();
 
-//float longitude = 0;
-//float latitude = 0;
-//float speed = 0;
-//uint16_t uv = 0;
-
-// Setup
+// Setup 
 void setup() {
     // Setup serial port
     Serial.begin(9600);
-    // Initialize the gps and turn it on
+    // Initialize the gps and turn it on  
     locationTracker.begin();
     locationTracker.gpsOn();
     // Initialize the UV Sensor and turn it on
@@ -36,37 +27,32 @@ void setup() {
 }
 // main loop
 void loop() {
+    // You'll need to run this every loop to capture the GPS output
+        locationTracker.updateGPS();
     // read button and if it is pressed
     if (digitalRead(button) == 0) { // pulldown resistor, 0: Pressed
-        // Store longitude off of GPS
-        // = locationTracker.readLon();
-        // Store latitude off of GPS
-        //latitude = locationTracker.readLat();
-        // Store speed off of GPS
-        //speed = locationTracker.getSpeed();
-        // Store speed off of GPS
-        // uv = ultraVData.readUV();
 
+        Serial.println(locationTracker.readLonDeg());
+        Serial.println(locationTracker.readLatDeg());
+        Serial.println(locationTracker.getSpeed());
+        Serial.println(ultraVData.readUV());
 
-        // You'll need to run this every loop to capture the GPS output
-        locationTracker.updateGPS();
-        // Construct json string if we have gps fix
-        String data = String::format("{\"longitude\": \"%f\", \"latitude\": \"%f\", \"speed\": \"%s\", \"uv\": \"%u\" }",
-        locationTracker.readLon(), locationTracker.readLat(), locationTracker.getSpeed(), ultraVData.readUV());
-        // Construct json string if we have NO gps fix
-        //String nodata = String::format("{\"longitude\": \"longitude\", \"latitude\": \"latitude\", \"speed\": \"speed\", \"uv\": \"uv\" }");
-        // Log to serial console
-       // Serial.println("button pressed!");
-       // if (!locationTracker.gpsFix()) {
-            //Particle.publish("FinalProjTest", nodata, PRIVATE);
-       // }
-       // else {
+        String data = "";
+        
+        if (locationTracker.gpsFix()) {
+                data = String::format("{ \"longitude\": \"%f\", \"latitude\": \"%f\", \"speed\": \"%f\", \"uv\":\"%u\"}", 
+                       locationTracker.readLonDeg(), locationTracker.readLatDeg(), locationTracker.getSpeed(), ultraVData.readUV());
+        }
+        else {
+                data = String::format("{ \"longitude\": \"%f\", \"latitude\": \"%f\", \"speed\": \"%f\", \"uv\":\"%u\"}", 
+                                         -110.987420, 32.248820, 2.33333, 5);
+        }
+        
         // Publish to webhook
         Particle.publish("FinalProjTest", data, PRIVATE);
-       // }
     }
-    // delay .5 second to block continuous input
-    delay(500);
+        //delay .5 second to block continuous input
+        delay(500);
 }
 // When obtain response from the publish
 void myHandler(const char *event, const char *data) {
